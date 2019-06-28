73bde409eeba8671c093370e91a04cf242772dab
Alberto Ramos <alberto.ramos@emc.com>
2019-06-28 17:41:12 +1000

    Update compare-faces.py

--- compare-faces.py
+++ compare-faces.py
@@ -2,8 +2,8 @@ import boto3
 import json
 import os
 
-aws_access_key_id = os.environ['ACCESS_KEY'],
-aws_secret_access_key = os.environ['SECRET_KEY'],
+aws_access_key_id = os.environ['ACCESS_KEY']
+aws_secret_access_key = os.environ['SECRET_KEY']
 region_name='us-west-2'
 
 rk = boto3.client('rekognition', region_name=region_name, aws_access_key_id = aws_access_key_id, aws_secret_access_key = aws_secret_access_key)
